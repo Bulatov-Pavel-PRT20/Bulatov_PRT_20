b9669c8ceeea699d7c0b3f41c73eb902111fa461
Bulatov-Pavel-PRT20 <71261690+Bulatov-Pavel-PRT20@users.noreply.github.com>
2023-10-01 13:23:26 +0500

    Update laba1.py

--- LAB1/laba1.py
+++ LAB1/laba1.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Load .png image
-image = cv2.imread('image.png')
+image = cv2.imread('scene.png')
 
 # Save .jpg image
-cv2.imwrite('image.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+cv2.imwrite('scene.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
